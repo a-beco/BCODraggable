db91f4e2a72e5403aad808285d8f504b7d632f4c
Kohei Abe <ecothm.abe@gmail.com>
2014-06-09 02:13:51 +0900

    Initial Commit

---
+++ Draggable/BCOAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  BCOAppDelegate.h
+//  Draggable
+//
+//  Created by 阿部耕平 on 2014/06/09.
+//  Copyright (c) 2014年 Kohei Abe. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BCOAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Draggable/BCOViewController.h
@@ -0,0 +1,13 @@
+//
+//  BCOViewController.h
+//  Draggable
+//
+//  Created by 阿部耕平 on 2014/06/09.
+//  Copyright (c) 2014年 Kohei Abe. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BCOViewController : UIViewController
+
+@end
